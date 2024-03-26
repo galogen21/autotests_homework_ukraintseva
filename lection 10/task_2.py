f76af99bac7de1d5de223e6569a2e93e6e823b48
galogen21 <131530459+galogen21@users.noreply.github.com>
2024-03-26 23:05:28 +0300

    Update task_2.py done

--- lection 10/task_2.py
+++ lection 10/task_2.py
@@ -15,3 +15,21 @@ def all_division(*arg1):
     for i in arg1[1:]:
         division /= i
     return division
+
+@pytest.mark.smoke
+def test_zero_exc():
+    with pytest.raises(ZeroDivisionError):
+        all_division(5, 0)
+
+@pytest.mark.smoke
+def test_ok1():
+    assert all_division(25, 5) == 5
+
+def test_ok2():
+    assert all_division(1872, 52) == 36
+
+def test_type_exc():
+    with pytest.raises(TypeError):
+        all_division('jjjj', 'rrrrrr')
+def test_ok_minus():
+    assert all_division(-30, 6) == -5

a327f0e762b122d6a6b280adca4333d697a5ec6a
galogen21 <131530459+galogen21@users.noreply.github.com>
2024-04-08 12:26:48 +0300

    Update task_1.py_DONE

--- lection 11/task_1.py
+++ lection 11/task_1.py
@@ -9,3 +9,26 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
+
+browser = webdriver.Chrome()
+browser.get('https://sbis.ru/')
+browser.maximize_window()
+try:
+    header = browser.find_element(By.CSS_SELECTOR, '[href="/contacts"]')
+    header.click()
+    tensor_site = browser.find_element(By.CSS_SELECTOR, '[href="https://tensor.ru/"]')
+    tensor_site.click()
+    handles = browser.window_handles
+    browser.switch_to.window(handles[1])
+    sleep(3)
+    article = browser.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__block4-content')
+    browser.execute_script("return arguments[0].scrollIntoView(true);", article)
+    assert article.is_displayed() == True, 'Элемент отсутствует!'
+    sleep(3)
+    tensor_about = browser.find_element(By.CSS_SELECTOR, '[href="/about"]')
+    tensor_about.click()
+    assert browser.current_url == 'https://tensor.ru/about', 'Неверный адрес'
+    sleep(5)
+finally:
+    browser.quit()
